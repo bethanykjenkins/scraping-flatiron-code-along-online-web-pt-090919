efd66ece0def2277164d7cc285a06db84958c584
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-11-02 14:32:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -7,6 +8,8 @@ class Scraper
   
  def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    binding.pry
   end
 
   def get_courses
@@ -35,5 +38,6 @@ class Scraper
 
 end
 
+Scraper.new.print_courses
 
 
